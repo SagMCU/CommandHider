62c13ebedf7a1c6498bbdf9f76d6fe6bece77767
SagMCU <52503815+SagMCU@users.noreply.github.com>
2025-03-05 20:36:14 +0100

    Now it works 1.8 - 1.21.4

--- CommandHider/src/main/java/com/sagmcu/CommandHider/Main.java
+++ CommandHider/src/main/java/com/sagmcu/CommandHider/Main.java
@@ -1,35 +1,84 @@
 package com.sagmcu.CommandHider;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sagmcu.CommandHider.commands.ReloadCommand;
+import com.sagmcu.CommandHider.filters.LegacyTabCompletionFilter;
+import com.sagmcu.CommandHider.filters.TabCompletionFilter;
 import com.sagmcu.CommandHider.utils.ChatColorTranslator;
-import com.sagmcu.CommandHider.utils.TabCompletionFilter;
 import com.sagmcu.CommandHider.bstats.Metrics;
 
 public class Main extends JavaPlugin {
-    private TabCompletionFilter tabCompletionFilter;
+    private Object tabFilter;
+    private String serverVersion;
+    private ChatColorTranslator chatColorTranslator;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        tabCompletionFilter = new TabCompletionFilter(this);
-        getServer().getPluginManager().registerEvents(tabCompletionFilter, this);
+        // Detect server version
+        serverVersion = getServer().getBukkitVersion().split("-")[0];
+        chatColorTranslator = new ChatColorTranslator(this);
+        // Load the appropriate filter based on version
+        if (isModernVersion()) {
+            tabFilter = new TabCompletionFilter(this);
+            getServer().getPluginManager().registerEvents((TabCompletionFilter) tabFilter, this);
+        } else {
+            // For 1.8-1.12, check for ProtocolLib and use LegacyTabCompletionFilter
+            Plugin protocolLib = getServer().getPluginManager().getPlugin("ProtocolLib");
+            if (protocolLib != null && protocolLib.isEnabled()) {
+                tabFilter = new LegacyTabCompletionFilter(this);
+                getLogger().info("ProtocolLib detected. Enabling tab completion filtering for 1.8 - 1.12.");
+            } else {
+                tabFilter = null; // Disable filtering if ProtocolLib is missing
+                getLogger().warning("ProtocolLib not found or not enabled. Tab completion filtering for 1.8 - 1.12 will not work.");
+            }
+        }
+
         getCommand("commandhider").setExecutor(new ReloadCommand(this));
         int pluginId = 24801;
         @SuppressWarnings("unused")
         Metrics metrics = new Metrics(this, pluginId);
-        String enabledMessage = ChatColorTranslator.translate("&aCommandHider &7has been &aenabled&7! &6:D");
+        String enabledMessage = chatColorTranslator.translate("&aCommandHider &7has been &aenabled&7! &6:D");
         this.getServer().getConsoleSender().sendMessage(enabledMessage);
     }
 
     @Override
     public void onDisable() {
-        String disabledMessage = ChatColorTranslator.translate("&cCommandHider &7has been &cdisabled&7! :(");
+        String disabledMessage = chatColorTranslator.translate("&cCommandHider &7has been &cdisabled&7! :(");
         this.getServer().getConsoleSender().sendMessage(disabledMessage);
     }
 
-    public TabCompletionFilter getTabCompletionFilter() {
-        return tabCompletionFilter;
+    public Object getTabFilter() {
+        return tabFilter;
+    }
+
+    public String getServerVersion() {
+        return serverVersion;
+    }
+
+    public boolean isModernVersion() {
+        try {
+            String[] versionParts = serverVersion.split("\\.");
+            int major = Integer.parseInt(versionParts[0]);
+            int minor = Integer.parseInt(versionParts[1]);
+            return major > 1 || (major == 1 && minor >= 13);
+        } catch (Exception e) {
+            return false; // Default to legacy if parsing fails
+        }
+    }
+
+    // Helper method to reload config for the active filter
+    public void reloadTabFilterConfig() {
+        if (isModernVersion()) {
+            ((TabCompletionFilter) tabFilter).reloadConfig();
+        } else {
+            ((LegacyTabCompletionFilter) tabFilter).reloadConfig();
+        }
+    }
+
+    public ChatColorTranslator getChatColorTranslator() { // Add getter
+        return chatColorTranslator;
     }
 }
