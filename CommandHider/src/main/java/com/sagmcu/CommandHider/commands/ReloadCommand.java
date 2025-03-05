419cd3923f76bb3c2c1cd8c949e14143b92a5fa2
SagMCU <52503815+SagMCU@users.noreply.github.com>
2025-03-05 20:37:26 +0100

    Now it works 1.8 - 1.21.4

--- CommandHider/src/main/java/com/sagmcu/CommandHider/commands/ReloadCommand.java
+++ CommandHider/src/main/java/com/sagmcu/CommandHider/commands/ReloadCommand.java
@@ -6,7 +6,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.sagmcu.CommandHider.Main;
-import com.sagmcu.CommandHider.utils.ChatColorTranslator;
 
 public class ReloadCommand implements CommandExecutor {
     private final Main plugin;
@@ -17,40 +16,26 @@ public class ReloadCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
         String reloadSuccessMessage = plugin.getConfig().getString("messages.reload_success");
         String noPermissionMessage = plugin.getConfig().getString("messages.no_permission");
 
-        reloadSuccessMessage = ChatColorTranslator.translate(reloadSuccessMessage);
-        noPermissionMessage = ChatColorTranslator.translate(noPermissionMessage);
+        reloadSuccessMessage = plugin.getChatColorTranslator().translate(reloadSuccessMessage);
+        noPermissionMessage = plugin.getChatColorTranslator().translate(noPermissionMessage);
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
-
-        if (!player.hasPermission("commandhider.admin")) {
-            player.sendMessage(noPermissionMessage);
-            return false;
+            if (!player.hasPermission("commandhider.admin")) {
+                player.sendMessage(noPermissionMessage);
+                return true;
+            }
         }
 
         if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
             plugin.reloadConfig();
-            if (plugin.getTabCompletionFilter() != null) {
-                plugin.getTabCompletionFilter().reloadConfig();
-            }
-            player.sendMessage(reloadSuccessMessage);
-            return true;
-        }
-    }
-        else {
-            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            plugin.reloadConfig();
-            if (plugin.getTabCompletionFilter() != null) {
-                plugin.getTabCompletionFilter().reloadConfig();
-            }
+            plugin.reloadTabFilterConfig();
             sender.sendMessage(reloadSuccessMessage);
             return true;
         }
-    }
 
         return false;
     }
